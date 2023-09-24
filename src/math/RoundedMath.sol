df1f41ac829f5c630465c7d9ca032de4d4276e62
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-09-24 18:25:56 -0400

    test and refactor RoundedMath

--- src/math/RoundedMath.sol
+++ src/math/RoundedMath.sol
@@ -5,8 +5,8 @@ uint256 constant WAD = 1e18;
 uint256 constant RAY = 1e27;
 
 library RoundedMath {
+    error MultiplicationOverflow(uint256 a, uint256 b);
     error DivisionByZero();
-    error MultiplicationOverflow();
 
     /**
      * @dev Multiplication with proper rounding as opposed to truncation.
@@ -15,13 +15,7 @@ library RoundedMath {
      * @return product in `WAD`
      */
     function roundedWadMul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0 || b == 0) return 0;
-
-        if (a > (type(uint256).max / b)) revert MultiplicationOverflow();
-
-        uint256 halfScale = WAD / 2;
-
-        return (a * b + halfScale) / WAD;
+        return roundedMul(a, b, WAD);
     }
 
     /**
@@ -31,12 +25,7 @@ library RoundedMath {
      * @return quotient in `WAD`
      */
     function roundedWadDiv(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (b == 0) revert DivisionByZero();
-        uint256 halfB = b / 2;
-
-        if (a > (type(uint256).max - halfB) / WAD) revert MultiplicationOverflow();
-
-        return (a * WAD + halfB) / b;
+        return roundedDiv(a, b, WAD);
     }
 
     /**
@@ -46,13 +35,7 @@ library RoundedMath {
      * @return product in `RAY`
      */
     function roundedRayMul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0 || b == 0) return 0;
-
-        if (a > (type(uint256).max / b)) revert MultiplicationOverflow();
-
-        uint256 halfScale = RAY / 2;
-
-        return (a * b + halfScale) / RAY;
+        return roundedMul(a, b, RAY);
     }
 
     /**
@@ -62,11 +45,39 @@ library RoundedMath {
      * @return quotient in `RAY`
      */
     function roundedRayDiv(uint256 a, uint256 b) internal pure returns (uint256) {
+        return roundedDiv(a, b, RAY);
+    }
+
+    /**
+     * @dev Multiplication with proper rounding as opposed to truncation.
+     * @param a multiplier
+     * @param b multiplicand
+     * @param scale of multiplicand
+     * @return product (in scale of multiplier)
+     */
+    function roundedMul(uint256 a, uint256 b, uint256 scale) internal pure returns (uint256) {
+        if (a == 0 || b == 0) return 0;
+
+        if (a > (type(uint256).max / b)) revert MultiplicationOverflow(a, b);
+
+        uint256 halfScale = scale / 2;
+
+        return (a * b + halfScale) / scale;
+    }
+
+    /**
+     * @dev Division with proper rounding as opposed to truncation.
+     * @param a dividend
+     * @param b divisor
+     * @param scale of divisor
+     * @return quotient (in scale of dividend)
+     */
+    function roundedDiv(uint256 a, uint256 b, uint256 scale) internal pure returns (uint256) {
         if (b == 0) revert DivisionByZero();
         uint256 halfB = b / 2;
 
-        if (a > (type(uint256).max - halfB) / RAY) revert MultiplicationOverflow();
+        if (a > (type(uint256).max - halfB) / scale) revert MultiplicationOverflow(a, b);
 
-        return (a * RAY + halfB) / b;
+        return (a * scale + halfB) / b;
     }
 }
