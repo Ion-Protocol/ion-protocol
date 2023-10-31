081b399616f9149e01ef165b495cfd1a08afb467
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-10-30 21:35:14 -0400

    fix: invariant test with foundry works after core refactor and echidna config selector whitelist was properly set up

--- test/invariant/IonPool/ActorManager.t.sol
+++ test/invariant/IonPool/ActorManager.t.sol
@@ -1,94 +1,220 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.21;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
 
-// import { IonPoolSharedSetup } from "../../helpers/IonPoolSharedSetup.sol";
-// import { IHevm } from "../../echidna/IHevm.sol";
-// import { LenderHandler, BorrowerHandler, LiquidatorHandler } from "./Handlers.t.sol";
-// import { safeconsole as console } from "forge-std/safeconsole.sol";
+import { GemJoin } from "src/join/GemJoin.sol";
+import { IonPool } from "src/IonPool.sol";
+import { RoundedMath } from "src/libraries/math/RoundedMath.sol";
 
-// import { CommonBase } from "forge-std/Base.sol";
-// import { StdCheats } from "forge-std/StdCheats.sol";
-// import { StdUtils } from "forge-std/StdUtils.sol";
+import { IonPoolSharedSetup } from "test/helpers/IonPoolSharedSetup.sol";
+import { IHevm } from "test/helpers/echidna/IHevm.sol";
 
-// contract ActorManager is CommonBase, StdCheats, StdUtils {
-//     IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+import { LenderHandler, BorrowerHandler, LiquidatorHandler } from "./Handlers.t.sol";
 
-//     LenderHandler[] internal lenders;
-//     BorrowerHandler[] internal borrowers;
-//     LiquidatorHandler[] internal liquidators;
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-//     constructor(
-//         LenderHandler[] memory _lenders,
-//         BorrowerHandler[] memory _borrowers,
-//         LiquidatorHandler[] memory _liquidators
-//     ) {
-//         lenders = _lenders;
-//         borrowers = _borrowers;
-//         liquidators = _liquidators;
-//     }
+import { safeconsole as console } from "forge-std/safeconsole.sol";
+import { CommonBase } from "forge-std/Base.sol";
+import { StdCheats } from "forge-std/StdCheats.sol";
+import { StdUtils } from "forge-std/StdUtils.sol";
 
-//     function supply(uint256 lenderIndex, uint256 amount) public {
-//         lenderIndex = bound(lenderIndex, 0, lenders.length - 1);
-//         lenders[lenderIndex].supply(amount);
-//     }
+using RoundedMath for uint256;
 
-//     function withdraw(uint256 lenderIndex, uint256 amount) public {
-//         lenderIndex = bound(lenderIndex, 0, lenders.length - 1);
-//         lenders[lenderIndex].withdraw(amount);
-//     }
-// }
+contract ActorManager is CommonBase, StdCheats, StdUtils {
+    IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-// contract IonPool_InvariantTest is IonPoolSharedSetup {
-//     IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    IonPool ionPool;
+    LenderHandler[] internal lenders;
+    BorrowerHandler[] internal borrowers;
+    LiquidatorHandler[] internal liquidators;
 
-//     uint256 internal constant AMOUNT_LENDERS = 4;
-//     uint256 internal constant AMOUNT_BORROWERS = 4;
-//     uint256 internal constant AMOUNT_LIQUDIATORS = 1;
+    constructor(
+        IonPool _ionPool,
+        LenderHandler[] memory _lenders,
+        BorrowerHandler[] memory _borrowers,
+        LiquidatorHandler[] memory _liquidators
+    ) {
+        ionPool = _ionPool;
+        lenders = _lenders;
+        borrowers = _borrowers;
+        liquidators = _liquidators;
+    }
 
-//     LenderHandler[] internal lenders;
-//     BorrowerHandler[] internal borrowers;
-//     LiquidatorHandler[] internal liquidators;
+    function supply(uint256 lenderIndex, uint256 amount) public {
+        lenderIndex = bound(lenderIndex, 0, lenders.length - 1);
+        lenders[lenderIndex].supply(amount);
+    }
 
-//     ActorManager public actorManager;
+    function withdraw(uint256 lenderIndex, uint256 amount) public {
+        lenderIndex = bound(lenderIndex, 0, lenders.length - 1);
+        lenders[lenderIndex].withdraw(amount);
+    }
 
-//     function setUp() public virtual override {
-//         // super.setUp();
-//         // for (uint256 i = 0; i < AMOUNT_LENDERS; i++) {
-//         //     LenderHandler lender = new LenderHandler(ionPool, underlying);
-//         //     lenders.push(lender);
-//         //     underlying.grantRole(underlying.MINTER_ROLE(), address(lender));
-//         // }
+    function borrow(uint256 borrowerIndex, uint256 ilkIndex, uint128 amount) public {
+        borrowerIndex = bound(borrowerIndex, 0, borrowers.length - 1);
+        ilkIndex = bound(ilkIndex, 0, ionPool.ilkCount() - 1);
 
-//         // for (uint256 i = 0; i < borrowers.length; i++) {
-//         //     borrowers.push(new BorrowerHandler(ionPool, ionHandler, underlying));
-//         //     for (uint256 j = 0; j < collaterals.length; j++) {
-//         //         collaterals[j].mint(address(borrowers[i]), INITIAL_BORROWER_COLLATERAL_BALANCE);
-//         //     }
-//         // }
+        borrowers[borrowerIndex].borrow(uint8(ilkIndex), amount);
+    }
 
-//         // actorManager = new ActorManager(lenders, borrowers, liquidators);
+    function repay(uint256 borrowerIndex, uint256 ilkIndex, uint128 amount) public {
+        borrowerIndex = bound(borrowerIndex, 0, borrowers.length - 1);
+        ilkIndex = bound(ilkIndex, 0, ionPool.ilkCount() - 1);
 
-//         // targetContract(address(actorManager));
-//         // ionPool.changeSupplyFactor(3.564039457584007913129639935e27);
-//     }
+        borrowers[borrowerIndex].repay(uint8(ilkIndex), amount);
+    }
 
-//     function invariant_lenderDepositsAddToBalance() external returns (bool) {
-//         for (uint256 i = 0; i < lenders.length; i++) {
-//             assertEq(lenders[i].totalHoldingsNormalized(), ionPool.normalizedBalanceOf(address(lenders[i])));
-//         }
-//         return !failed();
-//     }
+    function depositCollateral(uint256 borrowerIndex, uint256 ilkIndex, uint128 amount) public {
+        borrowerIndex = bound(borrowerIndex, 0, borrowers.length - 1);
+        ilkIndex = bound(ilkIndex, 0, ionPool.ilkCount() - 1);
 
-//     function invariant_lenderBalancesAddToTotalSupply() external returns (bool) {
-//         uint256 totalLenderNormalizedBalances;
-//         for (uint256 i = 0; i < lenders.length; i++) {
-//             totalLenderNormalizedBalances += ionPool.normalizedBalanceOf(address(lenders[i]));
-//         }
-//         assertEq(totalLenderNormalizedBalances, ionPool.normalizedTotalSupply());
-//         return !failed();
-//     }
+        borrowers[borrowerIndex].depositCollateral(uint8(ilkIndex), amount);
+    }
 
-//     // function invariant_tryProperty() external returns (bool) {
-//     //     return (ionPool.totalSupply() == 0);
-//     // }
-// }
+    function withdrawCollateral(uint256 borrowerIndex, uint256 ilkIndex, uint128 amount) public {
+        borrowerIndex = bound(borrowerIndex, 0, borrowers.length - 1);
+        ilkIndex = bound(ilkIndex, 0, ionPool.ilkCount() - 1);
+
+        borrowers[borrowerIndex].withdrawCollateral(uint8(ilkIndex), amount);
+    }
+
+    function gemJoin(uint256 borrowerIndex, uint256 ilkIndex, uint128 amount) public {
+        borrowerIndex = bound(borrowerIndex, 0, borrowers.length - 1);
+        ilkIndex = bound(ilkIndex, 0, ionPool.ilkCount() - 1);
+
+        borrowers[borrowerIndex].gemJoin(uint8(ilkIndex), amount);
+    }
+
+    function gemExit(uint256 borrowerIndex, uint256 ilkIndex, uint128 amount) public {
+        borrowerIndex = bound(borrowerIndex, 0, borrowers.length - 1);
+        ilkIndex = bound(ilkIndex, 0, ionPool.ilkCount() - 1);
+
+        borrowers[borrowerIndex].gemExit(uint8(ilkIndex), amount);
+    }
+}
+
+contract IonPool_InvariantTest is IonPoolSharedSetup {
+    IHevm hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    uint256 internal constant AMOUNT_LENDERS = 4;
+    uint256 internal constant AMOUNT_BORROWERS = 4;
+    uint256 internal constant AMOUNT_LIQUDIATORS = 1;
+
+    LenderHandler[] internal lenders;
+    BorrowerHandler[] internal borrowers;
+    LiquidatorHandler[] internal liquidators;
+
+    ActorManager public actorManager;
+
+    function setUp() public virtual override {
+        bool log = vm.envOr("LOG", uint256(0)) == 1;
+        _setUp(log);
+    }
+
+    function _setUp(bool log) internal {
+        super.setUp();
+
+        for (uint256 i = 0; i < AMOUNT_LENDERS; i++) {
+            LenderHandler lender = new LenderHandler(ionPool, underlying, log);
+            lenders.push(lender);
+            underlying.grantRole(underlying.MINTER_ROLE(), address(lender));
+        }
+
+        for (uint256 i = 0; i < AMOUNT_BORROWERS; i++) {
+            borrowers.push(new BorrowerHandler(ionPool, ionRegistry, underlying, mintableCollaterals, log));
+            for (uint256 j = 0; j < collaterals.length; j++) {
+                mintableCollaterals[j].grantRole(mintableCollaterals[j].MINTER_ROLE(), address(borrowers[i]));
+            }
+        }
+
+        // Disable debt ceiling
+        for (uint8 i = 0; i < ionPool.ilkCount(); i++) {
+            ionPool.updateIlkDebtCeiling(i, type(uint256).max);
+        }
+
+        actorManager = new ActorManager(ionPool, lenders, borrowers, liquidators);
+
+        targetContract(address(actorManager));
+        ionPool.setSupplyFactor(3.564039457584007913129639935e27);
+    }
+
+    function invariant_lenderDepositsAddToBalance() external returns (bool) {
+        for (uint256 i = 0; i < lenders.length; i++) {
+            assertEq(lenders[i].totalHoldingsNormalized(), ionPool.normalizedBalanceOf(address(lenders[i])));
+        }
+
+        return !failed();
+    }
+
+    function invariant_lenderBalancesAddToTotalSupply() external returns (bool) {
+        uint256 totalLenderNormalizedBalances;
+        for (uint256 i = 0; i < lenders.length; i++) {
+            totalLenderNormalizedBalances += ionPool.normalizedBalanceOf(address(lenders[i]));
+        }
+        assertEq(totalLenderNormalizedBalances, ionPool.normalizedTotalSupply());
+
+        return !failed();
+    }
+
+    function invariant_underlyingBalanceOfPoolPlusDebtToPoolStrictlyGreaterThanOrEqualToTotalSupply()
+        external
+        returns (bool)
+    {
+        uint256 totalDebt;
+        for (uint8 i = 0; i < ionPool.ilkCount(); i++) {
+            uint256 totalNormalizedDebts;
+            for (uint256 j = 0; j < borrowers.length; j++) {
+                totalNormalizedDebts += ionPool.normalizedDebt(i, address(borrowers[j]));
+            }
+            uint256 ilkRate = ionPool.rate(i);
+            totalDebt += totalNormalizedDebts.rayMulDown(ilkRate);
+        }
+        assertGe(ionPool.weth() + totalDebt, ionPool.totalSupply());
+
+        return !failed();
+    }
+
+    function invariant_borrowerNormalizedDebtsSumToTotalNormalizedDebt() external returns (bool) {
+        for (uint8 i = 0; i < ionPool.ilkCount(); i++) {
+            uint256 sumBorrowerNormalizedDebts;
+            for (uint256 j = 0; j < borrowers.length; j++) {
+                sumBorrowerNormalizedDebts += ionPool.normalizedDebt(i, address(borrowers[j]));
+            }
+            assertEq(sumBorrowerNormalizedDebts, ionPool.totalNormalizedDebt(i));
+        }
+
+        return !failed();
+    }
+
+    function invariant_sumOfAllGemAndCollateralEqualsBalanceOfGemJoin() external returns (bool) {
+        for (uint8 i = 0; i < ionPool.ilkCount(); i++) {
+            uint256 gemAndCollateralSum;
+            for (uint256 j = 0; j < borrowers.length; j++) {
+                gemAndCollateralSum += ionPool.gem(i, address(borrowers[j]));
+            }
+            for (uint256 j = 0; j < borrowers.length; j++) {
+                gemAndCollateralSum += ionPool.collateral(i, address(borrowers[j]));
+            }
+            GemJoin gemJoin = ionRegistry.gemJoins(i);
+            IERC20 gem = gemJoin.gem();
+            assertEq(gemAndCollateralSum, gem.balanceOf(address(gemJoin)));
+        }
+
+        return !failed();
+    }
+
+    function invariant_sumOfAllVaultNormalizedDebtEqualsIlkTotalNormalizedDebt() external returns (bool) {
+        for (uint8 i = 0; i < ionPool.ilkCount(); i++) {
+            uint256 sumVaultNormalizedDebt;
+            for (uint256 j = 0; j < borrowers.length; j++) {
+                sumVaultNormalizedDebt += ionPool.normalizedDebt(i, address(borrowers[j]));
+            }
+            assertEq(sumVaultNormalizedDebt, ionPool.totalNormalizedDebt(i));
+        }
+
+        return !failed();
+    }
+
+    function invariantFoundry_report() external returns (bool) {
+        return true;
+    }
+}
--- test/invariant/IonPool/Handlers.t.sol
+++ test/invariant/IonPool/Handlers.t.sol
@@ -1,17 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import { IonPoolExposed } from "../../helpers/IonPoolSharedSetup.sol";
-// import { IonHandler } from "../../../src/flash/IonHandler.sol";
-import { ERC20PresetMinterPauser } from "../../helpers/ERC20PresetMinterPauser.sol";
-import { IHevm } from "../../helpers/echidna/IHevm.sol";
-import { RoundedMath } from "../../../src/libraries/math/RoundedMath.sol";
+import { RoundedMath } from "src/libraries/math/RoundedMath.sol";
+import { IonRegistry } from "src/periphery/IonRegistry.sol";
+import { GemJoin } from "src/join/GemJoin.sol";
+
+import { IonPoolExposed } from "test/helpers/IonPoolSharedSetup.sol";
+import { ERC20PresetMinterPauser } from "test/helpers/ERC20PresetMinterPauser.sol";
+import { IHevm } from "test/helpers/echidna/IHevm.sol";
+
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { CommonBase } from "forge-std/Base.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
 import { StdUtils } from "forge-std/StdUtils.sol";
 
+import { safeconsole as console } from "forge-std/safeconsole.sol";
+
+// TODO: Clean up HEVM to be in one spot
 IHevm constant hevm = IHevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
 using RoundedMath for uint256;
@@ -19,26 +26,36 @@ using RoundedMath for uint256;
 abstract contract Handler is CommonBase, StdCheats, StdUtils {
     IonPoolExposed internal immutable ionPool;
     ERC20PresetMinterPauser internal immutable underlying;
+    bool immutable LOG;
 
-    constructor(IonPoolExposed _ionPool, ERC20PresetMinterPauser _underlying) {
+    constructor(IonPoolExposed _ionPool, ERC20PresetMinterPauser _underlying, bool _log) {
         ionPool = _ionPool;
         underlying = _underlying;
+        LOG = _log;
     }
 }
 
 contract LenderHandler is Handler {
     uint256 public totalHoldingsNormalized;
 
-    constructor(IonPoolExposed _ionPool, ERC20PresetMinterPauser _underlying) Handler(_ionPool, _underlying) {
+    constructor(
+        IonPoolExposed _ionPool,
+        ERC20PresetMinterPauser _underlying,
+        bool _log
+    )
+        Handler(_ionPool, _underlying, _log)
+    {
         underlying.approve(address(ionPool), type(uint256).max);
     }
 
     function supply(uint256 amount) public {
         amount = bound(amount, 0, type(uint128).max);
-        uint256 amountNormalized = amount.roundedRayDiv(ionPool.supplyFactor());
+        uint256 amountNormalized = amount.rayDivDown(ionPool.supplyFactor());
 
         if (amountNormalized == 0) return;
         totalHoldingsNormalized += amountNormalized;
+
+        if (LOG) console.log("supply", amount);
 
         underlying.mint(address(this), amount);
         ionPool.supply(address(this), amount, new bytes32[](0));
@@ -46,62 +63,145 @@ contract LenderHandler is Handler {
 
     function withdraw(uint256 amount) public {
         // To prevent reverts, limit withdraw amounts to the available liquidity in the pool
-        uint256 balance = Math.min(underlying.balanceOf(address(ionPool)), ionPool.balanceOf(address(this)));
+        uint256 balance = Math.min(ionPool.weth(), ionPool.balanceOf(address(this)));
         amount = bound(amount, 0, balance);
 
-        uint256 amountNormalized = amount.roundedRayDiv(ionPool.supplyFactor());
+        uint256 amountNormalized = amount.rayDivUp(ionPool.supplyFactor());
         if (amountNormalized == 0) return;
 
         totalHoldingsNormalized -= amountNormalized;
+
+        if (LOG) console.log("withdraw", amount);
 
         ionPool.withdraw(address(this), amount);
     }
 }
 
 contract BorrowerHandler is Handler {
-    // IonHandler internal immutable ionHandler;
+    IonRegistry immutable registry;
+    ERC20PresetMinterPauser[] collaterals;
 
     constructor(
         IonPoolExposed _ionPool,
-        // IonHandler _ionHandler,
-        ERC20PresetMinterPauser _underlying
+        IonRegistry _registry,
+        ERC20PresetMinterPauser _underlying,
+        ERC20PresetMinterPauser[] memory _collaterals,
+        bool _log
     )
-        Handler(_ionPool, _underlying)
+        Handler(_ionPool, _underlying, _log)
     {
-        // underlying.approve(address(ionPool), type(uint256).max);
-        // ionPool.hope(address(_ionHandler));
-        // ionHandler = _ionHandler;
+        underlying.approve(address(_ionPool), type(uint256).max);
+        ionPool.addOperator(address(_ionPool));
+        registry = _registry;
+        collaterals = _collaterals;
     }
 
-    // function borrow(uint8 ilkIndex, uint256 amount) public {
-    //     uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
-    //     ionHandler.borrow(_ilkIndex, amount);
-    // }
+    function borrow(uint8 ilkIndex, uint256 normalizedAmount) public {
+        uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
 
-    // function repay(address borrower, uint8 ilkIndex, uint256 amount) public {
-    //     uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
-    //     ionHandler.repay(_ilkIndex, amount);
-    // }
+        uint256 ilkRate = ionPool.rate(_ilkIndex);
+        uint256 maxAdditionalNormalizedDebt;
+        {
+            uint256 vaultCollateral = ionPool.collateral(_ilkIndex, address(this));
+            uint256 ilkSpot = ionPool.spot(_ilkIndex).getSpot();
+            uint256 vaultNormalizedDebt = ionPool.normalizedDebt(_ilkIndex, address(this));
 
-    // function modifyPosition(
-    //     address borrower,
-    //     uint8 ilkIndex,
-    //     address collateralSource,
-    //     address debtDestination,
-    //     int256 changeInCollateral,
-    //     int256 changeInNormalizedDebt
-    // )
-    //     public
-    // { }
+            uint256 currentDebt = vaultNormalizedDebt.rayMulDown(ilkRate);
+            uint256 debtLimit = ilkSpot.rayMulDown(vaultCollateral);
+            uint256 maxAdditionalDebt = debtLimit - currentDebt;
+            maxAdditionalNormalizedDebt = _min(maxAdditionalDebt.rayDivDown(ilkRate), type(uint64).max);
+        }
 
-    // function gemJoin(address borrower, uint8 ilkIndex, uint256 amount) public {
-    //     hevm.prank(borrower);
-    //     underlying.approve(address(ionPool), type(uint256).max);
-    //     hevm.prank(borrower);
-    //     ionPool.gemJoin(ilkIndex, amount);
-    // }
+        uint256 poolLiquidity = ionPool.weth();
+        uint256 normalizedPoolLiquidity = poolLiquidity.rayDivDown(ilkRate);
+
+        normalizedAmount = _min(bound(normalizedAmount, 0, maxAdditionalNormalizedDebt), normalizedPoolLiquidity);
+
+        if (LOG) console.log("borrow", normalizedAmount);
+
+        ionPool.borrow(_ilkIndex, address(this), address(this), normalizedAmount, new bytes32[](0));
+    }
+
+    function repay(uint8 ilkIndex, uint256 normalizedAmount) public {
+        uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
+
+        uint256 currentNormalizedDebt = ionPool.normalizedDebt(_ilkIndex, address(this));
+        normalizedAmount = bound(normalizedAmount, 0, currentNormalizedDebt);
+
+        if (LOG) console.log("repay", normalizedAmount);
+
+        ionPool.repay(_ilkIndex, address(this), address(this), normalizedAmount);
+    }
+
+    function depositCollateral(uint8 ilkIndex, uint256 amount) public {
+        uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
+
+        amount = bound(amount, 0, ionPool.gem(_ilkIndex, address(this)));
+
+        if (LOG) console.log("depositCollateral", amount);
+
+        ionPool.depositCollateral(_ilkIndex, address(this), address(this), amount, new bytes32[](0));
+    }
+
+    function withdrawCollateral(uint8 ilkIndex, uint256 amount) public {
+        uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
+
+        uint256 ilkRate = ionPool.rate(_ilkIndex);
+        uint256 maxRemovableCollateral;
+        {
+            uint256 vaultCollateral = ionPool.collateral(_ilkIndex, address(this));
+            uint256 ilkSpot = ionPool.spot(_ilkIndex).getSpot();
+            uint256 vaultNormalizedDebt = ionPool.normalizedDebt(_ilkIndex, address(this));
+
+            uint256 currentDebt = vaultNormalizedDebt.rayMulDown(ilkRate);
+            uint256 minimumCollateral = currentDebt.rayDivUp(ilkSpot);
+            maxRemovableCollateral = vaultCollateral - minimumCollateral;
+        }
+
+        amount = bound(amount, 0, maxRemovableCollateral);
+
+        if (LOG) console.log("withdrawCollateral", amount);
+
+        ionPool.withdrawCollateral(ilkIndex, address(this), address(this), amount);
+    }
+
+    function gemJoin(uint8 ilkIndex, uint256 amount) public {
+        uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
+
+        GemJoin _gemJoin = registry.gemJoins(_ilkIndex);
+        ERC20PresetMinterPauser gem = collaterals[_ilkIndex];
+        gem.approve(address(_gemJoin), amount);
+        gem.mint(address(this), amount);
+
+        amount = bound(amount, 0, gem.balanceOf(address(this)));
+
+        if (LOG) console.log("gemJoin", amount);
+
+        _gemJoin.join(address(this), amount);
+    }
+
+    function gemExit(uint8 ilkIndex, uint256 amount) public {
+        uint8 _ilkIndex = uint8(bound(ilkIndex, 0, ionPool.ilkCount()));
+
+        amount = bound(amount, 0, ionPool.gem(_ilkIndex, address(this)));
+        GemJoin _gemJoin = registry.gemJoins(_ilkIndex);
+
+        if (LOG) console.log("gemExit", amount);
+
+        _gemJoin.exit(address(this), amount);
+    }
+
+    function _min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
 }
 
 contract LiquidatorHandler is Handler {
-    constructor(IonPoolExposed _ionPool, ERC20PresetMinterPauser _underlying) Handler(_ionPool, _underlying) { }
+    constructor(
+        IonPoolExposed _ionPool,
+        ERC20PresetMinterPauser _underlying,
+        bool _log
+    )
+        Handler(_ionPool, _underlying, _log)
+    { }
 }
