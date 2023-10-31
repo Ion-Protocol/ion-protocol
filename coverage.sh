46e9132d0859f9f9d6380ac1c89a795dc9284a25
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-10-30 21:22:30 -0400

    chore: add coverage scripts that prune non-relevant dirs

---
+++ coverage.sh
@@ -0,0 +1,27 @@
+rm -rf coverage
+forge coverage --report lcov
+
+delete=false
+
+while read -r line; do
+    if [[ $line == SF* ]]; then
+        path=$(echo "$line" | cut -d':' -f2)
+        if [[ $path == test* || $path == script* ]]; then
+            delete=true
+            continue
+        fi
+    fi
+
+    if [[ $delete == true && $line == end_of_record* ]]; then
+        delete=false
+    fi
+
+    if [[ $delete == false ]]; then
+        echo "$line" >>lcov.info.pruned
+    fi
+
+done <lcov.info
+mv lcov.info.pruned lcov.info
+
+genhtml --branch-coverage --output "coverage" lcov.info
+open coverage/index.html
