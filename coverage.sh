d3d9ebf9bd7d726b92f2ce1513619480b6e0c7c9
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-11-05 16:59:30 -0500

    fix: make coverage script prune properly

--- coverage.sh
+++ coverage.sh
@@ -1,23 +1,30 @@
 rm -rf coverage
+rm -f lcov.info
 forge coverage --report lcov
 
 delete=false
 
 while read -r line; do
-    if [[ $line == SF* ]]; then
-        path=$(echo "$line" | cut -d':' -f2)
-        if [[ $path == test* || $path == script* || $path == src/libraries/uniswap* ]]; then
-            delete=true
-            continue
+    if [[ $line == TN* ]]; then
+        read -r line
+        if [[ $line == SF* ]]; then
+            path=$(echo "$line" | cut -d':' -f2)
+            if [[ $path == test* || $path == script* || $path == src/libraries/uniswap* ]]; then
+                delete=true
+                continue
+            fi
         fi
+    fi
+
+    if [[ $delete == false ]]; then
+        if [[ $line == SF* ]]; then
+            echo "TN:" >>lcov.info.pruned
+        fi
+        echo "$line" >>lcov.info.pruned
     fi
 
     if [[ $delete == true && $line == end_of_record* ]]; then
         delete=false
-    fi
-
-    if [[ $delete == false ]]; then
-        echo "$line" >>lcov.info.pruned
     fi
 
 done <lcov.info
