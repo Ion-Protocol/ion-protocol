0e90767ddb1e5145eb2ee747ee60862a2434d50d
Hrik Bhowal <hrik.bhowal@gmail.com>
2024-04-10 16:50:25 -0400

    feat: add yield ceiling on YieldOracle

--- test/fork/concrete/YieldOraclePendle.t.sol
+++ test/fork/concrete/YieldOraclePendle.t.sol
@@ -2,20 +2,24 @@
 pragma solidity 0.8.21;
 
 import { YieldOraclePendle } from "../../../src/YieldOraclePendle.sol";
-import { PT_WEETH_POOL, PT_RSWETH_POOL, PT_RSETH_POOL } from "../../../src/Constants.sol";
+import { PT_RSETH_POOL } from "../../../src/Constants.sol";
 
 import { Test } from "forge-std/Test.sol";
 
 contract YieldOraclePendle_Test is Test {
     YieldOraclePendle public oracle;
 
-    function setUp() public {
-        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
-        // PT_WEETH_POOL.increaseObservationsCardinalityNext(1000);
-        oracle = new YieldOraclePendle(PT_RSETH_POOL, 1800);
+    function test_apys() public {
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 19_625_980);
+        oracle = new YieldOraclePendle(PT_RSETH_POOL, 1800, 0.9e18);
+        assertEq(oracle.apys(0), 42_260_965);
     }
 
-    function test_apys() public {
-        assertEq(oracle.apys(0), 0);
+    function test_apysYieldCeiling() public {
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
+
+        // 2% is a low enough ceiling that it should always be hit
+        oracle = new YieldOraclePendle(PT_RSETH_POOL, 1800, 0.02e18);
+        assertEq(oracle.apys(0), 0.02e8);
     }
 }
